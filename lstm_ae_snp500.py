0e8b9be925032248c7fb4589934d0b0dfae1ef9b
roigol <roigol79@gmail.com>
2021-12-20 13:19:59 +0200

    task 3.3.1

--- lstm_ae_snp500.py
+++ lstm_ae_snp500.py
@@ -1,23 +1,30 @@
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+stocks = pd.read_csv('./SP 500 Stock Prices 2014-2017.csv')
+df_stocks_high = stocks.pivot(index='date', columns='symbol', values='high')
 
 
-def PtintGoogleAndAmazonMaximums():
-    stocks = pd.read_csv('./SP 500 Stock Prices 2014-2017.csv')
-    print(stocks.shape)
-    daily_max = stocks['high']
-    df_stocks_high = stocks.pivot(index='date', columns='symbol', values='high')
-    google = df_stocks_high['GOOGL']
-    amzn = df_stocks_high['AMZN']
-    print("hi")
+def PrintGoogleAndAmazonMaximums():
+    google = df_stocks_high['GOOGL'].values
+    amzn = df_stocks_high['AMZN'].values
+    date = df_stocks_high.index
     axis = [i for i in range(1007)]
-    plt.plot(axis, google, label='Google')
-    plt.plot(axis, amzn, label='Amazon')
-    plt.xlabel('Days from 2/1/2014')
-    plt.ylabel('Price')
+
+    df = pd.DataFrame({'values': np.random.randint(0, 1000, 1007)},
+                      index=pd.date_range(start='2014-01-02', end='2017-12-29', periods=1007))
+    fig, ax1 = plt.subplots()
+    plt.plot(df.index, google, label='Google')
+    plt.plot(df.index, amzn, label='Amazon')
+    monthyearFmt = mdates.DateFormatter('%Y %B')
+    ax1.xaxis.set_major_formatter(monthyearFmt)
+    _ = plt.xticks(rotation=90)
     plt.title('Google and Amazon daily maximums')
+    plt.ylabel('Price')
     plt.legend()
     plt.show()
 
 
+# PrintGoogleAndAmazonMaximums()
