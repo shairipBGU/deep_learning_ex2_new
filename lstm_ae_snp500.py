8656ed8557b4bda2a4de4857de0391b6dcaf2186
roigol <roigol79@gmail.com>
2021-12-21 12:25:22 +0200

    divide data to train and test

--- lstm_ae_snp500.py
+++ lstm_ae_snp500.py
@@ -1,26 +1,20 @@
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import torch
-import torchvision
-import torchvision.transforms as transforms
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
-import os
 
 from lstm_ae_toy import LSTMAE
 
-sequence_param = 1007
+sequence_param = 19
 grad_clip = 1
-learning_rate = 0.01
+learning_rate = 0.001
 hidden_state_size_param = 512
-input_size_param = 1
-epoch = 20
-batch_size = 5
-hidden_state = 512
+input_size_param = 53
+epoch = 200
+batch_size = 10
 PATH = './snp500_net.pth'
 
 stocks = pd.read_csv('./SP 500 Stock Prices 2014-2017.csv')
@@ -28,10 +22,16 @@ df_stocks_high = stocks.pivot(index='date', columns='symbol', values='high').dro
 df = pd.DataFrame({'values': np.random.randint(0, 1000, 1007)},
                   index=pd.date_range(start='2014-01-02', end='2017-12-29', periods=1007))
 
-data = torch.FloatTensor(np.asarray(df_stocks_high.values).T)
-data = data[:, :, None]
+transposed_data = np.asarray(df_stocks_high.values).T
+reshaped_data = np.reshape(transposed_data, (477, sequence_param, input_size_param))
+to_normalized_data = torch.FloatTensor(reshaped_data)
+data = torch.nn.functional.normalize(to_normalized_data)
 
-loader = torch.utils.data.DataLoader(dataset=data,
+# train set 2/3, test set 1/3
+train_set, test_set = torch.utils.data.random_split(data, [318, 159])
+
+
+loader = torch.utils.data.DataLoader(dataset=train_set,
                                      batch_size=batch_size,
                                      shuffle=True)
 data_iter = iter(loader)
@@ -71,7 +71,7 @@ def train(lstmae):
             losses.append(curr_loss)
             iterations.append((curr_epoch + i))
             if i % 20 == 19:  # print every 20 mini-batches
-                print('[%d, %5d] loss: %.3f' %
+                print('[%d, %5d] loss: %.6f' %
                       (curr_epoch + 1, i + 1, running_loss / 20))
                 running_loss = 0.0
 
@@ -84,6 +84,7 @@ def task_3_3_2():
     train(lstmae)
 
     test_data = data
+    test_data = test_set
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=True, num_workers=0)
 
     dataiter = iter(test_loader)
@@ -93,8 +94,6 @@ def task_3_3_2():
     original = stock.flatten().detach().numpy()
     plt.plot(df.index, original, label='original stock')
     plt.suptitle('Original Vs. Reconstructed Stock')
-    # plt.suptitle("Signal Value vs Time")
-    # plt.xlabel('Time')
     plt.ylabel('Price')
 
     lstmae.load_state_dict(torch.load(PATH))
